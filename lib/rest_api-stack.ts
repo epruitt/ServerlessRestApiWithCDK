b797411641e2e0a74fca2507ff97c3a5f85b8a51
epruitt <epruitt223@gmail.com>
2026-05-08 00:22:24 -0500

    created dynamoDB Table and grant lambda permission to read and write to it

--- lib/rest_api-stack.ts
+++ lib/rest_api-stack.ts
@@ -4,17 +4,33 @@ import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, Billing, TableV2 } from "aws-cdk-lib/aws-dynamodb";
 
 export class RestApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    //setting up the dynamodb table
+    const employeesTable = new TableV2(this, "empTable", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      billing: Billing.onDemand(),
+    });
 
     //setting up the Lambda
     const emplLambda = new NodejsFunction(this, "emplLambda", {
       runtime: Runtime.NODEJS_20_X,
       handler: "handler",
       entry: join(__dirname, "..", "services", "handler.ts"),
+      environment: {
+        TABLE_NAME: employeesTable.tableName,
+      },
     });
+
+    //granting lambda permission to read and write to dynamodb
+    employeesTable.grantReadWriteData(emplLambda);
 
     //Creating api gateway
     const api = new RestApi(this, "empAPI");
