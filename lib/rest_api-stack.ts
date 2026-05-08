58ef392ee83f618a077c90c7275d7b815a130d13
epruitt <epruitt223@gmail.com>
2026-05-07 23:55:42 -0500

    initial commit

--- lib/rest_api-stack.ts
+++ lib/rest_api-stack.ts
@@ -1,16 +1,30 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class RestApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //setting up the Lambda
+    const emplLambda = new NodejsFunction(this, "emplLambda", {
+      runtime: Runtime.NODEJS_20_X,
+      handler: "handler",
+      entry: join(__dirname, "..", "services", "handler.ts"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RestApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Creating api gateway
+    const api = new RestApi(this, "empAPI");
+    const emplResource = api.root.addResource("empl");
+
+    //Creating lambda integration
+    const emplLambdaIntegration = new LambdaIntegration(emplLambda);
+
+    //Adding get anf post method
+    emplResource.addMethod("GET", emplLambdaIntegration);
+    emplResource.addMethod("POST", emplLambdaIntegration);
   }
 }
