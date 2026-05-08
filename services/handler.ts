e06ba06aed1181111fdbcbdf5ca89a75e09712e7
epruitt <epruitt223@gmail.com>
2026-05-08 00:41:51 -0500

    added Get and Post functions

--- services/handler.ts
+++ services/handler.ts
@@ -3,15 +3,36 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { postEmpl } from "./postEmpl.js";
+import { getEmpl } from "./getEmpl";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+const ddbClient = new DynamoDBClient({});
 
 async function handler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Hello World!"),
-  };
+  let response: APIGatewayProxyResult = {} as any;
+
+  try {
+    switch (event.httpMethod) {
+      case "GET":
+        const getResponse = await getEmpl(event, ddbClient);
+        response = getResponse;
+        break;
+      case "POST":
+        const postResponse = await postEmpl(event, ddbClient);
+        response = postResponse;
+        break;
+    }
+  } catch (error) {
+    response = {
+      statusCode: 400,
+      body: JSON.stringify((error as Error).message),
+    };
+  }
+  return response;
 }
 
 export { handler };
